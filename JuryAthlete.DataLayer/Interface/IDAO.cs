34ecafb9b2161fed640fdeef0418ea9f4de9246b
Krishan <krishan_ahlawat@yahoo.com>
2017-08-17 08:21:20 +0200

    EF^ Added

---
+++ JuryAthlete.DataLayer/Interface/IDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuryAthlete.DataLayer.Interface
+{
+    /// <summary>
+    /// A generic repository pattern that wraps the IDbSet interface.
+    /// </summary>
+    /// <typeparam name="T"> Is a class derived from <see cref="EntityBase"/> class.</typeparam>
+    public interface IDAO<T> where T : class
+    {
+        IList<T> GetList(params Expression<Func<T, object>>[] navigationProperties);
+        IList<T> GetList(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties);
+        T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties);
+        bool Add(params T[] items);
+        bool Update(params T[] items);
+        bool Remove(params T[] items);
+    }
+}
